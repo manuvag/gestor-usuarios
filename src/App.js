ba381ed7ac2a86637a5b1d7633c88146ad262354
Emmanuel Valenzuela <emmanuel@kukulha.com>
2021-11-02 02:06:15 -0600

    Refactor rendimiento

--- src/App.js
+++ src/App.js
@@ -1,59 +1,28 @@
 import { useState } from 'react'
-import useFormulario from './hooks/useFormulario'
-import Input from './components/Input'
 import Card from './components/Card'
 import Container from './components/Container'
-import Button from './components/Button'
+import UserForm from './components/UserForm'
 
 const App = () => {
   const [usuarios, setUsuarios] = useState([])
-  const [formulario, handleChange, reset] = useFormulario({ 
-    name: '', 
-    lastname:'', 
-    email: '' 
-  })
 
-  const submit = (e) => {
-    e.preventDefault()
+  const submit =  usuario => {
     setUsuarios([
       ...usuarios,
-      formulario,
+      usuario,
     ])
-    reset()
   }
+
   return(
     <div style={{padding:20}}>
       <Container>
         <Card>
-          <form onSubmit={submit}>
-            <Input 
-              label='Nombre' 
-              name='name' 
-              value={formulario.name} 
-              onChange={handleChange}
-              placeholder='Nombre'
-            />
-            <Input 
-              label='Apellido' 
-              name='lastname' 
-              value={formulario.lastname} 
-              onChange={handleChange}
-              placeholder='Apellido'
-            />
-            <Input 
-              label='Email' 
-              name='email' 
-              value={formulario.email} 
-              onChange={handleChange}
-              placeholder='Email'
-            />
-            <Button>Enviar</Button>
-          </form>
+          <UserForm submit={submit}/>
         </Card>
         <Card>
           <ul>
-            {usuarios.map(usuario => 
-            <li key={usuario.email}>{`${usuario.name} ${usuario.lastname}: ${usuario.email}`}</li>
+            {usuarios.map(x => 
+            <li key={x.email}>{`${x.name} ${x.lastname}: ${x.email}`}</li>
             )}
           </ul>
         </Card>
